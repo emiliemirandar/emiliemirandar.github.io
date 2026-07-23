df3f2c0cf73c262311af50ee20c96e20f7fdb70c
Emilie <120046392+mirandaemilie@users.noreply.github.com>
2026-07-23 19:02:53 -0400

    Update script.js

--- dmvfarms/script.js
+++ dmvfarms/script.js
@@ -9,25 +9,25 @@
 
 async function loadData() {
   try {
-    // Replace w/ chosen API
-    // Examples:
-    // const response = await fetch('https://data.princegeorgescountymd.gov/resource/xxxx.json');
-    // const response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY');
-    // const data = await response.json();
 
     const response = await fetch ('database.json')
     const data = await response.json();
     console.log("data loaded", data);
 
     return data;
-    
-    // Simulate API delay
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    // return mockRestaurantData;
+  
   } catch (error) {
     console.error("Failed to load data:", error);
     throw new Error("Could not load data from API");
   }
 }
+
+async function init() {
+  const farms = await loadData();       
+  const html = farms.map(farm => renderFarmCard(farm)).join("");
+  document.getElementById("root").innerHTML = html;
+}
+
+init();
 
 // export default loadData
